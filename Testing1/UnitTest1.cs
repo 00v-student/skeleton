8ba6baee6dfbc9aaa6af664c2d3feb96c392fd2e
Vertika <P2644930@my365.dmu.ac.uk>
2023-02-22 13:53:30 +0000

    stock

---
+++ Testing1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsWatch Awatch = new clsWatch();
+            Assert.isNotNull(Awatch);
+        }
+    }
+}
