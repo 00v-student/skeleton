4c7ca3da5f273c722792c504c3fc25e7f12943e3
Vertika <P2644930@my365.dmu.ac.uk>
2023-02-22 16:35:43 +0000

    stock props added

---
+++ ClassLibrary/clsStock.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ClassLibrary
+{
+    public class clsStock
+    {
+        public bool available { get; set; }
+        public DateTime dateadd { get; set; }
+        public int supplierid { get; set; }
+        public string desc { get; set; }
+        public float price { get; set; }
+        public int stock { get; set; }
+    }
+}
