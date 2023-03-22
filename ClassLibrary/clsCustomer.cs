5bdda1f32906335aeaf74eac3a3da047d93b88e6
P2517528 <P2517528@CLPX0027WIN017W>
2023-03-22 19:21:01 +0000

    added testing for customer class

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,6 +1,20 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public bool VerifiedAccount { get; set; }
+        public string Gender { get; set; }
+
+        public string Valid(string firstname, string lastname, string dateofbirth, string phonenumber, string emailaddress, string verifiedaccount, string gender)
+        {
+            return "";
+        }
     }
 }
--- Testing6/tstCustomer.cs
+++ Testing6/tstCustomer.cs
@@ -7,6 +7,14 @@ namespace Testing6
     [TestClass]
     public class tstCustomer
     {
+         string firstname = "Lauren";
+         string lastname = "Jenkins";
+         string dateofbirth = DateTime.Now.ToString();
+         string phonenumber = "07875847364";
+         string emailaddress = "laurenjen43@outlook.com";
+         string verifiedaccount = bool.TrueString;
+         string gender = "M";
+
         [TestMethod]
         public void InstanceOK()
         {
@@ -14,6 +22,78 @@ namespace Testing6
             clsCustomer AnCustomer = new clsCustomer();
             //test to see that it exists
             Assert.IsNotNull(AnCustomer);
+        }
+
+        [TestMethod]
+        public void FirstNameOK()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            string TestFirstName = "Samantha";
+            AnCustomer.FirstName = TestFirstName;
+            Assert.AreEqual(AnCustomer.FirstName, TestFirstName);
+        }
+
+        [TestMethod]
+        public void LastNameOK()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            string TestLastName = "Jones";
+            AnCustomer.LastName = TestLastName;
+            Assert.AreEqual(AnCustomer.LastName, TestLastName);
+        }
+        public void DateOfBirthOK()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            DateTime TestDateOfBirth = DateTime.Now;
+            AnCustomer.DateOfBirth = TestDateOfBirth;
+            Assert.AreEqual(AnCustomer.DateOfBirth, TestDateOfBirth);
+        }
+
+        [TestMethod]
+        public void PhoneNumberOK()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            string TestPhoneNumber = "07754385754";
+            AnCustomer.PhoneNumber = TestPhoneNumber;
+            Assert.AreEqual(AnCustomer.PhoneNumber, TestPhoneNumber);
+        }
+
+        [TestMethod]
+        public void EmailAddressOK()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            string TestEmail = "samanthajo21@gmail.com";
+            AnCustomer.EmailAddress = TestEmail;
+            Assert.AreEqual(AnCustomer.EmailAddress, TestEmail);
+        }
+
+        [TestMethod]
+        public void VerifiedAccountOK()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            bool TestVerifiedAccount = true;
+            AnCustomer.VerifiedAccount = TestVerifiedAccount;
+            Assert.AreEqual(AnCustomer.VerifiedAccount, TestVerifiedAccount);
+        }
+
+        [TestMethod]
+        public void GenderOK()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            string TestGender = "M";
+            AnCustomer.Gender = TestGender;
+            Assert.AreEqual(AnCustomer.Gender, TestGender);
+
+        }
+
+        [TestMethod]
+        public void ValidOK()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            string Error = "";
+            Error = AnCustomer.Valid(firstname, lastname, dateofbirth, phonenumber, emailaddress, verifiedaccount, gender);
+            Assert.AreEqual(Error, "");
+
         }
     }
 }
