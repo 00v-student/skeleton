f16621006fc41499ef2d509a01ba1cca4d3958cf
Thomas Keown <P2538888@my365.dmu.ac.uk>
2023-05-03 02:24:00 +0100

    added uodate method and add method

--- AdminSystem/OrderDataEntry.aspx.cs
+++ AdminSystem/OrderDataEntry.aspx.cs
@@ -18,57 +18,32 @@ public partial class _1_DataEntry : System.Web.UI.Page
     {
         //create a new instance of the clsOrders
         clsOrders AnOrder = new clsOrders();
-
         //capture the Order Number
-        AnOrder.OrderNumber = Convert.ToInt32(txtOrderNumber.Text);
-        //store the address in the session object
-        Session["AnOrder"] = AnOrder;
-        //navigate to the viewer page
-        Response.Redirect("OrderViewer.aspx");
-    
-
+        string OrderNumber = txtOrderNumber.Text;
         //capture the Order description
-        AnOrder.OrderDescription = txtOrderDescription.Text;
-        //store the address in the session object
-        Session["AnOrder"] = AnOrder;
-        //navigate to the viewer page
-        Response.Redirect("OrderViewer.aspx");
-
-        //capture the Order Date
-        AnOrder.OrderDate = Convert.ToDateTime(txtOrderDate.Text);
-        //store the address in the session object
-        Session["AnOrder"] = AnOrder;
-        //navigate to the viewer page
-        Response.Redirect("OrderViewer.aspx");
-
+        string OrderDescription = txtOrderDescription.Text;
         //capture the Order Total
-        AnOrder.OrderTotal = Convert.ToInt32(txtOrderTotal.Text);
-        //store the address in the session object
-        Session["AnOrder"] = AnOrder;
-        //navigate to the viewer page
-        Response.Redirect("OrderViewer.aspx");
-
-        //capture the Order Complete
-        AnOrder.Active = chkOrderComplete.Checked;
-        //store the address in the session object
-        Session["AnOrder"] = AnOrder;
-        //navigate to the viewer page
-        Response.Redirect("OrderViewer.aspx");
-
+        string OrderTotal = txtOrderTotal.Text;
+        //capture the Order Date
+        string OrderDate = txtOrderDate.Text;
         //capture the Customer ID
-        AnOrder.CustomerID = Convert.ToInt32(txtOrderTotal.Text);
-        //store the address in the session object
-        Session["AnOrder"] = AnOrder;
-        //navigate to the viewer page
-        Response.Redirect("OrderViewer.aspx");
+        string CustomerId = txtCustomerID.Text;
 
         //capture the Staff Name
-        AnOrder.StaffName = txtOrderDescription.Text;
-        //store the address in the session object
-        Session["AnOrder"] = AnOrder;
-        //navigate to the viewer page
-        Response.Redirect("OrderViewer.aspx");
+        string StaffName = txtStaffName.Text;
+        string Error = "";
+        Error = AnOrder.Valid(OrderNumber, OrderDescription, OrderTotal, OrderDate, CustomerId);
+        if (Error == "")
+        {
+            AnOrder.OrderDescription = OrderDescription;
+            AnOrder.OrderDate = Convert.ToDateTime(OrderDate);
 
+            clsOrderCollection OrderList = new clsOrderCollection();
+            OrderList.ThisOrders = AnOrder;
+            OrderList.Add();
+
+            Response.Redirect("OrderList.aspx");
+        }
     }
 
     protected void txtStaffName_TextChanged(object sender, EventArgs e)
